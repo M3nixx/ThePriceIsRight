75cc8a2751805a19552d80039017df220e21c0f8
Tobi <t.endt@ostfalia.de>
2024-05-05 18:20:46 +0200

    Preise in den ItemInfo

--- static/js/ItemInfo.js
+++ static/js/ItemInfo.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let currentPage = 0;
 async function fetchItemByGtin(gtin) {
     const config = {
         method: 'GET',
@@ -12,7 +13,18 @@ async function fetchItemByGtin(gtin) {
     const item = await response.json();
     return item;
 }
-
+async function fetchReceipts(gtin, page, pagesize) {
+    const config = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'X-API-Key': 'afdb55d3-aa85-42c9-a2fc-fa3e378b04b5'
+        }
+    }
+    const response = await fetch(`http://trawl-fki.ostfalia.de/api/data/item/${gtin}?page=${page}&size=${pagesize}`, config); // Verwenden Sie die gtin im API-Link
+    const receipts = await response.json();
+    return receipts;
+}
 async function fetchImageByUri(uri) {
     const config = {
         method: 'GET',
@@ -31,7 +43,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     const searchParam = searchParams.get('gtin');
     const ids = ["name", "description", "gtin", "product", "value", "unit", "producer", "image"];
     const store = await fetchItemByGtin(searchParam);
-
+    console.log(await fetchReceipts(searchParam, 0,10));
+    const receipts = await fetchReceipts(searchParam, 0, 10);
     let i = 0;
 
     document.getElementById(ids[i++]).textContent = store.name;
@@ -44,4 +57,38 @@ document.addEventListener('DOMContentLoaded', async () => {
     const img = fetchImageByUri(store.image);
     console.log(img);
     document.getElementById(ids[i++]).textContent = store.image;
+    displayPreis(receipts);
+    updatePaginationLabel();
+
+    document.getElementById("prev").addEventListener("click", async () => {
+        if (currentPage > 0) {
+            currentPage--;
+            displayPreis(await fetchReceipts(searchParam, currentPage, 10));
+            updatePaginationLabel();
+        }
+    });
+
+    document.getElementById("next").addEventListener("click", async () => {
+        if (currentPage < receipts.info.totalPages - 1) {
+            currentPage++;
+            displayPreis(await fetchReceipts(searchParam, currentPage, 10));
+            updatePaginationLabel();
+        }
+    });
 });
+
+function displayPreis(receipts){
+    var tbody = document.querySelector('#preise tbody');
+    tbody.innerHTML = '';
+    receipts.content.forEach(function(receipt) {
+        var row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${receipt.store}</td>
+            <td>${receipt.price}</td>
+        `;
+        tbody.appendChild(row);
+    });
+}
+function updatePaginationLabel() {
+    document.querySelector('label').textContent = `Seite ${currentPage + 1}`;
+}
