d51ed9a12a5006ca6ed7290031c1c81177f3f505
id844247 <le.mendel@ostfalia.de>
2024-05-28 21:00:36 +0200

    HomePage redirect/ responsive HomePage

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const port = 8080;
 app.use(express.static('static'));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.redirect('/HomePage.html');
+});
+
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
 });
